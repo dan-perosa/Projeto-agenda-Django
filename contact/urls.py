08101145a8ddb75c6b37395215ab9cea01c21a31
Dan <dane.gcp@gmail.com>
2024-04-17 11:10:07 -0300

    Commit projeto finalizado

---
+++ contact/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from contact import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+app_name = 'contact'
+
+urlpatterns = [
+    path('search/', views.search, name='search'),
+    path('', views.index, name='index'),
+
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+
+    path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+    path('user/update/', views.user_update, name='user_update'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
